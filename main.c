2d2e641a44ae58c53771adf3b3d2106f5a70a375
gauranga18 <sauravjyotikalita56@gmail.com>
2025-11-11 22:36:04 +0530

    Added Project to the repository

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include<stdlib.h>
 int main(){
-    int arr[]={};
-    
+    int arr[]={10,20,30,40,50};
+    int *ptr = arr;
+    ptr = (int *)malloc(20);
+    for (int i = 0;i<5;i++){
+        printf("Address -> %p\n", (void*)&arr[i]);
+        printf("Value -> %d\n", arr[i]);
+    }
     return 0;
 }
